67b4d6c7fd76fb477084993c3b030fa721a7ce7d
Kuba Bartwicki <kuba.bartwicki@digital.cabinet-office.gov.uk>
2018-06-18 10:49:14 +0100

    mock-up for archiving

--- app/routes.js
+++ app/routes.js
@@ -6,21 +6,21 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-router.post('/ur/did-you-award', function (req, res) {
+router.post('/did-you-award', function (req, res) {
   var radiogroup = req.body.radiogroup
   if (radiogroup == 'yes') {
-    res.redirect('/ur/award-info')
+    res.redirect('/award-info')
   } else if (radiogroup == 'no') {
-    res.redirect('/ur/why-not')
+    res.redirect('/why-not')
   } else {
-    res.redirect('/ur?step=4')
+    res.redirect('/summary?step=4')
   }
 })
-router.post('/ur/why-not', function (req, res) {
-  res.redirect('/ur?step=5&not_awarded=1&awarded_flash=1')
+router.post('/why-not', function (req, res) {
+  res.redirect('/summary?step=5&not_awarded=1&awarded_flash=1')
 })
-router.post('/ur/award-details', function (req, res) {
-  res.redirect('/ur?step=5&awarded_flash=1&not_awarded=0')
+router.post('/award-details', function (req, res) {
+  res.redirect('/summary?step=5&awarded_flash=1&not_awarded=0')
 })
 
 module.exports = router
